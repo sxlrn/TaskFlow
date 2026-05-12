5beef16a78a2ef33ab71bbaa4d2c9d02105f4fde
sxlrn <sashakalagurka662@gmail.com>
2026-05-12 22:06:03 +0300

    .

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_GOOGLE_CLIENT_ID': JSON.stringify('882588794900-9ev9i6bcetou2fgqa4p5rkrr3ihc79vu.apps.googleusercontent.com'),
+  },
 })
