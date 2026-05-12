89896f8fdde75b40e11b5592cf37049d822ee421
sxlrn <sashakalagurka662@gmail.com>
2026-05-12 23:10:31 +0300

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -17,7 +17,9 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(helmet());
+app.use(helmet({
+  crossOriginOpenerPolicy: false,
+}));
 app.use(cors({
   origin: [
     'https://kursova-taskflow.vercel.app',
